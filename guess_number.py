8a63ff0024f6411832866289b43f5a8ad9fcdeb4
Charles <linkin8288@gmail.com>
2020-09-08 09:46:27 +0200

    add counts

--- guess_number.py
+++ guess_number.py
@@ -2,17 +2,22 @@
 # 讓使用者重複輸入數字去猜
 # 猜對的話 印出'出於猜對了'
 # 猜錯的話 印出'比答案大/小'
+# 開始計數
 
 import random # 載入隨機模組
 
 r = random.randint(1, 100) #用函式產生隨機數
+count = 0 # 不能寫在迴圈裡面，不然會一直歸零
 while True:
+	count += 1 # count = count = count + 1
 	num = input('請猜數字: ') #input都會把東西存成字串
 	num = int(num) #將字串轉換成整數
 	if num == r:
 		print('你猜中了!')
+		print('這是你猜的', count, '次')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')	
+	print('這是你猜的', count, '次') # 若寫在if裡面，就要寫三次，這樣只要寫一次就好
